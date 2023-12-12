92422c1dae043e3891706bc3bb532bec3a05b099
BuildTools <unconfigured@null.spigotmc.org>
2023-12-12 17:44:18 +0100

    Added Faq page

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,15 @@
+import Section10 from "@/components/sections/Section10/Section10";
+import Section11 from "@/components/sections/Section11/Section11";
+import Section8 from "@/components/sections/Section8/Section8";
+
+const Faq = () => {
+  return (
+    <>
+      <Section8></Section8>
+      <Section10></Section10>
+      <Section11></Section11>
+    </>
+  );
+};
+
+export default Faq;
