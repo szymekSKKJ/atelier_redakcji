250b884af50ca6bf6d7a702a17499209faf529c5
BuildTools <unconfigured@null.spigotmc.org>
2023-12-21 19:05:12 +0100

    Added sub menu

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -9,6 +9,8 @@ import hamburger from "../../../public/hamburger.svg";
 import hamburgerClose from "../../../public/hamburger_close.svg";
 import { useEffect, useState } from "react";
 import Button from "../UI/Button/Button";
+import arrowDown from "../../../public/arrow.svg";
+import arrowDownBlue from "../../../public/arrow down blue.svg";
 
 const Navigation = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -39,6 +41,17 @@ const Navigation = () => {
       content: "Oferta",
       styles: [],
       href: "/offer",
+      subLinks: [
+        { id: 1, content: "Prace licencjackie", href: "/offer/prace-licenjcackie" },
+        { id: 2, content: "Prace inżynierskie", href: "/offer/prace-inzynierskie" },
+        { id: 3, content: "Prace magisterskie", href: "/offer/prace-magisterskie" },
+        { id: 4, content: "Prace doktorskie i habilitacyjne", href: "/offer/prace-doktorskie-i-habilitacyjne" },
+        { id: 5, content: "Prace zaliczeniowe", href: "/offer/prace-zaliczeniowe" },
+        { id: 6, content: "Prace dyplomowe", href: "/offer/prace-dyplomowe" },
+        { id: 7, content: "Prace naukowe", href: "/offer/publikacje-naukowe" },
+        { id: 8, content: "Teksty specjalistyczne", href: "/offer/teksty-specjalistyczne" },
+        { id: 9, content: "inne teksty", href: "/offer/inne-teksty" },
+      ],
     },
     {
       id: 5,
@@ -94,19 +107,36 @@ const Navigation = () => {
         <div className={`${styles.wrapper}`}>
           {links.map((linkData, index) => {
             if (index > 0) {
-              const { id, content, styles: stylesLocal, href } = linkData;
+              const { id, content, styles: stylesLocal, href, subLinks } = linkData;
 
               const formattedStyles = stylesLocal.join(" ");
 
+              const isActive = href === "/" ? pathname === href : pathname.includes(href.split("/").splice(-1, 1).join(""));
+
               return (
-                <Link
-                  key={id}
-                  href={href}
-                  className={`${formattedStyles} ${
-                    href === "/" ? (pathname === href ? styles.active : "") : pathname.includes(href.split("/").splice(-1, 1).join("")) ? styles.active : ""
-                  }`}>
-                  {content}
-                </Link>
+                <div className={`${styles.link_wrapper}`} key={id}>
+                  <Link href={href} className={`${formattedStyles} ${isActive ? styles.active : ""}`}>
+                    {content}{" "}
+                    {subLinks && (
+                      <Image
+                        style={isActive ? { transform: "rotate(90deg)" } : {}}
+                        src={isActive ? arrowDown : arrowDownBlue}
+                        alt="Ikonka strzałki w dół"></Image>
+                    )}
+                  </Link>
+                  {subLinks && (
+                    <div className={`${styles.sub_menu}`}>
+                      {subLinks.map((linkData) => {
+                        const { id, content, href } = linkData;
+                        return (
+                          <Link href={href} key={id}>
+                            {content}
+                          </Link>
+                        );
+                      })}
+                    </div>
+                  )}
+                </div>
               );
             }
           })}
@@ -117,14 +147,30 @@ const Navigation = () => {
       <nav className={`${styles.mobile}  ${isMobileMenuOpen ? styles.open : ""}`}>
         {links.map((linkData, index) => {
           if (index > 0) {
-            const { id, content, styles: stylesLocal, href } = linkData;
+            const { id, content, styles: stylesLocal, href, subLinks } = linkData;
 
             const formattedStyles = stylesLocal.join(" ");
 
+            const isActive = href === "/" ? pathname === href : pathname.includes(href.split("/").splice(-1, 1).join(""));
+
             return (
-              <Link key={id} href={href} className={`${formattedStyles} ${pathname === href ? styles.active : ""}`} onClick={() => setIsMobileMenuOpen(false)}>
-                {content}
-              </Link>
+              <div key={id}>
+                <Link href={href} className={`${formattedStyles} ${isActive ? styles.active : ""}`} onClick={() => setIsMobileMenuOpen(false)}>
+                  {content}
+                </Link>
+                {subLinks && (
+                  <div className={`${styles.sub_menu}`}>
+                    {subLinks.map((linkData) => {
+                      const { id, content, href } = linkData;
+                      return (
+                        <Link href={href} key={id} onClick={() => setIsMobileMenuOpen(false)}>
+                          {content}
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
             );
           }
         })}
