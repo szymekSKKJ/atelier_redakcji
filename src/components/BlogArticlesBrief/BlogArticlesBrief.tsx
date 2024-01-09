672d4a6186cb335a42b969abe87efabbb23776b5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 20:58:15 +0100

    Added whole blog article biref as a link

--- src/components/BlogArticlesBrief/BlogArticlesBrief.tsx
+++ src/components/BlogArticlesBrief/BlogArticlesBrief.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Button from "../UI/Button/Button";
 import styles from "./styles.module.scss";
 import Image from "next/image";
@@ -21,31 +22,35 @@ const BlogArticlesBrief = ({ articles }: componentProps) => {
           if (articles.length === 1) {
             return (
               <article key={id}>
-                <div className={`${styles.image_wrapper}`} role="img" aria-label="Zdjęcie">
-                  <Image height={512} width={512} src={image} alt="Zdjęcie artykułu bloga"></Image>
-                </div>
-                <div className={`${styles.content_wrapper}`}>
-                  <h2>{title}</h2>
-                  <p className={`${styles.brief}`} dangerouslySetInnerHTML={{ __html: brief }}></p>
-                  <Button theme="transparent-blue" style={{ padding: "20px 30px 20px 30px", fontSize: "16px" }} changeRoute={`/blog/${id}`}>
-                    Czytaj więcej
-                  </Button>
-                </div>
+                <Link href={`/blog/${id}`}>
+                  <div className={`${styles.image_wrapper}`} role="img" aria-label="Zdjęcie">
+                    <Image height={512} width={512} src={image} alt="Zdjęcie artykułu bloga"></Image>
+                  </div>
+                  <div className={`${styles.content_wrapper}`}>
+                    <h2>{title}</h2>
+                    <p className={`${styles.brief}`} dangerouslySetInnerHTML={{ __html: brief }}></p>
+                    <Button theme="transparent-blue" style={{ padding: "20px 30px 20px 30px", fontSize: "16px" }} changeRoute={`/blog/${id}`}>
+                      Czytaj więcej
+                    </Button>
+                  </div>
+                </Link>
               </article>
             );
           } else {
             return (
               <article key={id}>
-                <figure>
-                  <div className={`${styles.wrapper}`} role="img" aria-label="Zdjęcie">
-                    <Image height={512} width={512} src={image} alt="Zdjęcie artykułu bloga"></Image>
-                  </div>
-                  <figcaption>{title}</figcaption>
-                </figure>
-                <p dangerouslySetInnerHTML={{ __html: brief }}></p>
-                <Button theme="transparent-blue" style={{ padding: "20px 30px 20px 30px", fontSize: "16px" }} changeRoute={`/blog/${id}`}>
-                  Czytaj więcej
-                </Button>
+                <Link href={`/blog/${id}`}>
+                  <figure>
+                    <div className={`${styles.wrapper}`} role="img" aria-label="Zdjęcie">
+                      <Image height={512} width={512} src={image} alt="Zdjęcie artykułu bloga"></Image>
+                    </div>
+                    <figcaption>{title}</figcaption>
+                  </figure>
+                  <p dangerouslySetInnerHTML={{ __html: brief }}></p>
+                  <Button theme="transparent-blue" style={{ padding: "20px 30px 20px 30px", fontSize: "16px" }} changeRoute={`/blog/${id}`}>
+                    Czytaj więcej
+                  </Button>
+                </Link>
               </article>
             );
           }
