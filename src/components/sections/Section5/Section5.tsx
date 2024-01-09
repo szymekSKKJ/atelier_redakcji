49ef53030b81ef9e2b9bfcc5c500add04977b8c3
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 20:09:15 +0100

    Removed animated counter animation from section5

--- src/components/sections/Section5/Section5.tsx
+++ src/components/sections/Section5/Section5.tsx
@@ -1,92 +1,36 @@
-"use client";
 import styles from "./styles.module.scss";
-import { AnimatedCounter } from "react-animated-counter";
-import { useEffect, useRef, useState } from "react";
 import CurvedLines from "@/design/CurvedLines/CurvedLines";
 
-// Zmienić value of counter gdy ta sekcja będzie widoczna #SKKJ
-
 const Section5 = () => {
-  const [isSectionIntersecting, setIsSectionIntersecting] = useState(false);
-
-  const intersectionObserverRef = useRef<null | IntersectionObserver>(null);
-  const sectionElementRef = useRef<HTMLElement | null>(null);
-
-  useEffect(() => {
-    if (intersectionObserverRef.current === null) {
-      intersectionObserverRef.current = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            setIsSectionIntersecting(entry.isIntersecting);
-          });
-        },
-        {
-          root: null,
-          threshold: 0,
-        }
-      );
-
-      intersectionObserverRef.current.observe(sectionElementRef.current!);
-    }
-  }, []);
-
   return (
-    <section className={`${styles.section}`} ref={sectionElementRef}>
+    <section className={`${styles.section}`}>
       <h2>Atelier redakcji tekstu w liczbach</h2>
       <CurvedLines></CurvedLines>
       <div className={`${styles.articles}`} role="group">
         <article>
           <div className={`${styles.animated_counter}`} role="group">
-            <AnimatedCounter
-              fontSize="56px"
-              color="#fbfbfb"
-              incrementColor="#fbfbfb"
-              decrementColor="#fbfbfb"
-              value={isSectionIntersecting ? 6 : 0}
-              decimalPrecision={0}></AnimatedCounter>
-            +
+            <p>6+</p>
           </div>
           <p>LAT</p>
           <p>doświadczenia</p>
         </article>
         <article>
           <div className={`${styles.animated_counter}`} role="group">
-            <AnimatedCounter
-              fontSize="56px"
-              color="#fbfbfb"
-              incrementColor="#fbfbfb"
-              decrementColor="#fbfbfb"
-              value={isSectionIntersecting ? 12 : 0}
-              decimalPrecision={0}></AnimatedCounter>
-            +
+            <p>12+</p>
           </div>
           <p>REDAKTORÓW</p>
           <p>w zespole</p>
         </article>
         <article>
           <div className={`${styles.animated_counter}`} role="group">
-            <AnimatedCounter
-              fontSize="56px"
-              color="#fbfbfb"
-              incrementColor="#fbfbfb"
-              decrementColor="#fbfbfb"
-              value={isSectionIntersecting ? 880 : 0}
-              decimalPrecision={0}></AnimatedCounter>
-            +
+            <p>880+</p>
           </div>
           <p>ZADOWOLONYCH</p>
           <p>klientów</p>
         </article>
         <article>
           <div className={`${styles.animated_counter}`} role="group">
-            <AnimatedCounter
-              fontSize="56px"
-              color="#fbfbfb"
-              incrementColor="#fbfbfb"
-              decrementColor="#fbfbfb"
-              value={isSectionIntersecting ? 54000 : 0}
-              decimalPrecision={0}></AnimatedCounter>
-            +
+            <p>54 000+</p>
           </div>
           <p>ZREDAGOWANYCH</p>
           <p>korekt A4</p>
