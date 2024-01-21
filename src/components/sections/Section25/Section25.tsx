739dd2b328df8979e275b41b025026a6efb941bc
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 15:13:01 +0100

    Added images to contact form in contact page

--- src/components/sections/Section25/Section25.tsx
+++ src/components/sections/Section25/Section25.tsx
@@ -3,10 +3,16 @@ import arrowDown from "../../../../public/arrow down (Stroke).svg";
 import Image from "next/image";
 import CurvedLines from "@/design/CurvedLines/CurvedLines";
 import CustomOfferForm from "../../Forms/CustomOfferForm/CustomOfferForm";
+import bgImage1 from "../../../../public/customOfferForm/Contact Form BG 1.png";
+import bgImage2 from "../../../../public/customOfferForm/Contact Form BG 2.png";
 
 const Section25 = () => {
   return (
     <section className={`${styles.section}`}>
+      <div className={`${styles.background}`}>
+        <Image src={bgImage1} alt="Zdjęcie tła"></Image>
+        <Image src={bgImage2} alt="Zdjęcie tła"></Image>
+      </div>
       <Image src={arrowDown} alt="Duża ikona strzałki w dół" className={`${styles.arrow_down}`}></Image>
       <h2>Masz jakieś pytania?</h2>
       <CurvedLines></CurvedLines>
