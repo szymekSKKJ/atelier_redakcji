3e9d2cfc1e51fdd353862564fe67533281d81c41
BuildTools <unconfigured@null.spigotmc.org>
2024-03-27 16:43:17 +0100

    Added SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Mulish } from "next/font/google";
 import "./global.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const mulishFont = Mulish({ subsets: ["latin"] });
 
@@ -10,8 +11,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <script src="https://kit.fontawesome.com/845eb6a366.js" async></script>
       </head>
-      <body className={mulishFont.className}>{children}</body>
-      <Analytics />
+      <body className={mulishFont.className}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
