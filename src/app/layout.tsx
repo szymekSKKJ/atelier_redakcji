3d4b3f24c08a4512e85cbe2ec3c09282fcc64c33
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 13:38:45 +0100

    Changed lang of html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const mulishFont = Mulish({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <head>
         <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, target-densitydpi=device-dpi" />
         <script src="https://kit.fontawesome.com/845eb6a366.js" async></script>
